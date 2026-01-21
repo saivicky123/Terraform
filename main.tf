e0585d5db7d5ba0c174344f19d8468546b592571
Vignesh C <reddyvignesh1024@gmail.com>
2026-01-21 16:56:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
-provider "local" {
-}
+resource "local_file" "my_pet" {
+  filename = "pets.txt"
+  content  = "my dog name is max"
+  }
 
-resource "local_file" "terraform" {
-  filename = "terraform.txt"
-  content  = "terraform-jenkins integration"
+resource "random_pet" "petname" {
+  prefix = "MRS"
+  separator = "."
+  length = "1"
 }
